3bdbf9d5926e336a36c48ced904d3ccd5215acd9
Alec Barlow <abarlow85@gmail.com>
2017-01-13 15:07:00 -0800

    add POST /users/login

--- server/server.js
+++ server/server.js
@@ -130,6 +130,20 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+
+	const {email, password} = req.body;
+
+	User.findByCredentials(email, password).then(user => {
+		return user.generateAuthToken().then(token => {
+			res.header('x-auth', token).send(user)
+		});
+	}).catch(e => {
+		res.status(400).send();
+	});
+
+});
+
 app.listen(PORT, () => {
 	console.log('Started on port', PORT)
 })
