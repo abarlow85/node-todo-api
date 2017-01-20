7ab2115e5fef23f92e0b2789837157e8c56c45c6
Alec Barlow <abarlow85@gmail.com>
2017-01-20 14:24:10 -0800

    cors

--- server/server.js
+++ server/server.js
@@ -4,12 +4,20 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+
+
 const {mongoose} = require('./db/mongoose');
 
 const app = express();
+
 const PORT = process.env.PORT;
+const corsOptions = {
+	origin: ['http://localhost:3000']
+};
 
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 require('./routes/index')(app);
 
 app.listen(PORT, () => {
